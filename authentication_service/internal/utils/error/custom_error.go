02e94eaa427400c23ac54a2c617e8aba5d91314a
federico <federicoberto708@gmail.com>
2025-05-11 11:09:19 +0700

    refactor : move error variable in regex.go to custom_error.go

--- authentication_service/internal/utils/error/custom_error.go
+++ authentication_service/internal/utils/error/custom_error.go
@@ -2,19 +2,23 @@ package error
 
 import (
 	"errors"
-	"github.com/RecruitEase-Capstone/recruitEase-BE/authentication_service/internal/utils/regex"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
 var (
-	ErrInvalidCredentials = errors.New("invalid email or password")
-	ErrUserNotFound       = errors.New("user not found")
-	ErrEmailExist         = errors.New("email already exist")
-	ErrNotVerified        = errors.New("account has not been verified")
-	ErrIncorrectPassword  = errors.New("incorrect password")
-	ErrDatabase           = errors.New("database error")
-	ErrRowsAffected       = errors.New("error due to there is no or more than 1 affected column")
+	ErrInvalidCredentials     = errors.New("invalid email or password")
+	ErrUserNotFound           = errors.New("user not found")
+	ErrEmailExist             = errors.New("email already exist")
+	ErrNotVerified            = errors.New("account has not been verified")
+	ErrIncorrectPassword      = errors.New("incorrect password")
+	ErrDatabase               = errors.New("database error")
+	ErrRowsAffected           = errors.New("error due to there is no or more than 1 affected column")
+	ErrPasswordTooShort       = errors.New("password must be at least 8 characters long")
+	ErrPasswordMissingLower   = errors.New("password must contain at least one lowercase letter")
+	ErrPasswordMissingUpper   = errors.New("password must contain at least one uppercase letter")
+	ErrPasswordMissingNumber  = errors.New("password must contain at least one number")
+	ErrPasswordMissingSpecial = errors.New("password must contain at least one special character")
 )
 
 func MapErrorToStatus(err error) error {
@@ -25,12 +29,11 @@ func MapErrorToStatus(err error) error {
 		return status.Error(codes.NotFound, "user not found")
 	case errors.Is(err, ErrInvalidCredentials):
 		return status.Error(codes.Unauthenticated, "invalid email or password")
-	// mapping regex errors
-	case errors.Is(err, regex.ErrPasswordTooShort),
-		errors.Is(err, regex.ErrPasswordMissingLower),
-		errors.Is(err, regex.ErrPasswordMissingUpper),
-		errors.Is(err, regex.ErrPasswordMissingNumber),
-		errors.Is(err, regex.ErrPasswordMissingSpecial):
+	case errors.Is(err, ErrPasswordTooShort),
+		errors.Is(err, ErrPasswordMissingLower),
+		errors.Is(err, ErrPasswordMissingUpper),
+		errors.Is(err, ErrPasswordMissingNumber),
+		errors.Is(err, ErrPasswordMissingSpecial):
 		return status.Error(codes.InvalidArgument, err.Error())
 	default:
 		return status.Error(codes.Internal, "internal server error")
